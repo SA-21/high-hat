5b136eeabc3ae8fe76f5411a02a9a40ccb4c0acc
SA_21 <safg0421@cau.ac.kr>
2020-05-29 17:05:43 +0900

    add Login ,Sign up

---
+++ high_hat/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+import challenge.views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    path('', challenge.views.home, name="home"),
+    path('challenges/', challenge.views.challenges, name="challenges"),
+    path('sample/', challenge.views.sample, name="sample"),
+    path('login/', challenge.views.login, name="login"),
+    path('signup/', challenge.views.signup, name="signup"),
+    path('dashboard/', challenge.views.dashboard, name="dashboard"),
+]
